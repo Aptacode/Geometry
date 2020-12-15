8358f2c508d71b121c28488672f5b2437d9d3aa6
Electricschmidt <tomdschmidt@gmail.com>
2020-12-15 18:24:16 +0000

    Added in PolyLine/PolyLine fine collision

--- Aptacode.Geometry/Collision/FineCollisionDetector.cs
+++ Aptacode.Geometry/Collision/FineCollisionDetector.cs
@@ -49,26 +49,128 @@ namespace Aptacode.Geometry.Collision
 
         #region PolyLine
 
-        public override bool CollidesWith(PolyLine p1, Point p2) => false;
+        public override bool CollidesWith(PolyLine p1, Point p2)
+        {
+            foreach (var (v1, v2) in p1.LineSegments())
+            {
+                if (!Helpers.OnLineSegment((v1, v2), p2.Position))
+                {
+                    continue;
+                }
 
-        public override bool CollidesWith(PolyLine p1, PolyLine p2) => false;
+                return true;
+            }
+
+            return false;
+        }
+
+        public override bool CollidesWith(PolyLine p1, PolyLine p2)
+        {
+            foreach(var (v1, v2) in p1.LineSegments())
+            {
+                foreach(var (v3, v4) in p2.LineSegments())
+                {
+                    if(Helpers.LineSegmentIntersection((v1, v2), (v3, v4)))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
 
         public override bool CollidesWith(PolyLine p1, Polygon p2) => false;
 
-        public override bool CollidesWith(PolyLine p1, Circle p2) => false;
+        public override bool CollidesWith(PolyLine p1, Circle p2)
+        {
+            foreach (var (v1, v2) in p1.LineSegments())
+            {
+                if (p2.BoundingCircle.Contains(v1) || p2.BoundingCircle.Contains(v2))
+                {
+                    return true;
+                }
+
+                var dot = ((p2.Position.X - v1.X) * (v2.X - v1.X) + (p2.Position.Y - v1.Y) * (v2.Y - v1.Y)) /
+                          (v2 - v1).LengthSquared();
+                var closestX = v1.X + dot * (v2.X - v1.X);
+                var closestY = v1.Y + dot * (v2.Y - v1.Y);
+                var closestPoint =
+                    new Vector2(closestX,
+                        closestY); //The point of intersection of a line from the center of the circle perpendicular to the line segment (possibly the ray) with the line segment (or ray).
+                if (!Helpers.OnLineSegment((v1, v2), closestPoint)
+                ) //Closest intersection point may be beyond the ends of the line segment.
+                {
+                    return false;
+                }
+
+                if (p2.BoundingCircle.Contains(closestPoint)
+                ) //Closest intersection point is inside the circle means circle intersects the line.
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
 
         #endregion
 
         #region Polygon
 
-        public override bool CollidesWith(Polygon p1, Point p2) => false;
+        public override bool CollidesWith(Polygon p1, Point p2) 
+        {
+            var collision = false;
+            var edges = p1.Edges();
+            var point = p2.Position;
+            foreach (var (a, b) in edges)
+            {
+                if ((a.Y >= point.Y && b.Y < point.Y || a.Y < point.Y && b.Y >= point.Y) &&
+                    point.X < (b.X - a.X) * (point.Y - a.Y) / (b.Y - a.Y) + a.X)
+                {
+                    collision = !collision;
+                }
+            }
 
+
+            return collision;
+        }
         public override bool CollidesWith(Polygon p1, PolyLine p2) => false;
 
         public override bool CollidesWith(Polygon p1, Polygon p2) => false;
 
-        public override bool CollidesWith(Polygon p1, Circle p2) => false;
+        public override bool CollidesWith(Polygon p1, Circle p2)
+        {
+            {
+                foreach (var (v1, v2) in p1.Edges())
+                {
+                    if (p2.BoundingCircle.Contains(v1) || p2.BoundingCircle.Contains(v2))
+                    {
+                        return true;
+                    }
 
+                    var dot = ((p2.Position.X - v1.X) * (v2.X - v1.X) + (p2.Position.Y - v1.Y) * (v2.Y - v1.Y)) /
+                              (v2 - v1).LengthSquared();
+                    var closestX = v1.X + dot * (v2.X - v1.X);
+                    var closestY = v1.Y + dot * (v2.Y - v1.Y);
+                    var closestPoint =
+                        new Vector2(closestX,
+                            closestY); //The point of intersection of a line from the center of the circle perpendicular to the edge (possibly the ray) with the line segment (or ray).
+                    if (!Helpers.OnLineSegment((v1, v2), closestPoint)
+                    ) //Closest intersection point may be beyond the ends of the edge.
+                    {
+                        return false;
+                    }
+
+                    if (p2.BoundingCircle.Contains(closestPoint)
+                    ) //Closest intersection point is inside the circle means circle intersects the edge.
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
         #endregion
 
         #region Circle
--- Aptacode.Geometry/Collision/Helpers.cs
+++ Aptacode.Geometry/Collision/Helpers.cs
@@ -12,5 +12,31 @@ namespace Aptacode.Geometry.Collision
             const float buffer = 0.1f; //useful so that you don't have to literally be right on the line.
             return d1 + d2 >= lineLength - buffer && d1 + d2 <= lineLength + buffer;
         }
+
+        public static bool LineSegmentIntersection((Vector2, Vector2) l1, (Vector2, Vector2) l2)
+        {
+            if((l1.Item1 ==  l2.Item1 || l1.Item2 == l2.Item2) || l1.Item1 == l2.Item2 || l1.Item2 == l1.Item1) //These lines has at least one of the same endpoints
+            {
+                return true;
+            }
+            var det = (l2.Item2.Y - l2.Item1.Y) * (l1.Item2.X - l1.Item1.X) - (l2.Item2.X - l2.Item1.X) * (l1.Item2.Y - l2.Item1.Y);
+            if(det == 0) //These lines are parallel and do not intersect
+            {
+                return false;
+            }
+
+            var A = ((l2.Item2.X - l2.Item1.X) * (l1.Item1.Y - l2.Item1.Y) - (l2.Item2.Y - l2.Item1.Y) * (l1.Item1.X - l2.Item1.X)) / det;
+            var B = ((l1.Item2.X - l1.Item1.X) * (l1.Item1.Y - l2.Item1.Y) - (l1.Item2.Y - l1.Item1.Y) * (l1.Item1.X - l2.Item1.X)) / det;
+
+            if(A >= 0 && A <=1 && B >= 0 && B <= 1)
+            {
+                return true;
+            }
+
+            //var xIntersect = l1.Item1.X + A * (l1.Item2.X - l1.Item1.X)
+            //var yIntersect = l1.Item1.Y + B * (l1.Item2.Y - l1.Item1.Y)
+
+            return false;
+        }
     }
 }
