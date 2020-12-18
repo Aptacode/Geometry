177fc323ec86d61b12a738d1e0ee1fbc751e49ce
Timothy Jones <t.jones@aptacode.com>
2020-12-18 21:30:31 +0000

    Added PrimitiveGroup Transformations

--- Aptacode.Geometry/Composites/PrimitiveGroupExtensions.cs
+++ Aptacode.Geometry/Composites/PrimitiveGroupExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Numerics;
 using Aptacode.Geometry.Primitives;
 
 namespace Aptacode.Geometry.Composites
@@ -16,6 +17,62 @@ namespace Aptacode.Geometry.Composites
         {
             var children = group.Children.ToList();
             children.Remove(child);
+            return new PrimitiveGroup(children);
+        }
+
+        public static PrimitiveGroup Translate(this PrimitiveGroup group, Primitive child, Vector2 delta)
+        {
+            var children = group.Children.ToList();
+            var childIndex = children.IndexOf(child);
+            if (childIndex == -1)
+            {
+                return group;
+            }
+            
+            children.RemoveAt(childIndex);
+            children.Insert(childIndex, child.Translate(delta));
+            return new PrimitiveGroup(children);
+        }
+
+        public static PrimitiveGroup Rotate(this PrimitiveGroup group, Primitive child, Vector2 rotationCenter, float theta)
+        {
+            var children = group.Children.ToList();
+            var childIndex = children.IndexOf(child);
+            if (childIndex == -1)
+            {
+                return group;
+            }
+
+            children.RemoveAt(childIndex);
+            children.Insert(childIndex, child.Rotate(rotationCenter, theta));
+            return new PrimitiveGroup(children);
+        }
+
+        public static PrimitiveGroup Scale(this PrimitiveGroup group, Primitive child, Vector2 delta)
+        {
+            var children = group.Children.ToList();
+            var childIndex = children.IndexOf(child);
+            if (childIndex == -1)
+            {
+                return group;
+            }
+
+            children.RemoveAt(childIndex);
+            children.Insert(childIndex, child.Scale(delta));
+            return new PrimitiveGroup(children);
+        }
+        
+        public static PrimitiveGroup Skew(this PrimitiveGroup group, Primitive child, Vector2 delta)
+        {
+            var children = group.Children.ToList();
+            var childIndex = children.IndexOf(child);
+            if (childIndex == -1)
+            {
+                return group;
+            }
+
+            children.RemoveAt(childIndex);
+            children.Insert(childIndex, child.Skew(delta)));
             return new PrimitiveGroup(children);
         }
     }
