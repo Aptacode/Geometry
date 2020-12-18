4b271b09aba8f513f8d4fff5e7ed940f7365e42e
Timothy Jones <t.jones@aptacode.com>
2020-12-18 21:24:02 +0000

    Added PrimitiveGroupExtensions

---
+++ Aptacode.Geometry/Composites/PrimitiveGroupExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Aptacode.Geometry.Primitives;
+
+namespace Aptacode.Geometry.Composites
+{
+    public static class PrimitiveGroupExtensions
+    {
+        public static PrimitiveGroup Add(this PrimitiveGroup group, Primitive child)
+        {
+            var children = group.Children.ToList();
+            children.Add(child);
+            return new PrimitiveGroup(children);
+        }
+
+        public static PrimitiveGroup Remove(this PrimitiveGroup group, Primitive child)
+        {
+            var children = group.Children.ToList();
+            children.Remove(child);
+            return new PrimitiveGroup(children);
+        }
+    }
+}
