19aa8f58d91bacf7b64ef7628c8bbc5d8c28d0bc
Tim <t.jones@timmoth.com>
2022-02-28 00:17:34 +0000

    Fixed broken line intersection method

--- Geometry/Collision/Helpers.cs
+++ Geometry/Collision/Helpers.cs
@@ -39,25 +39,65 @@ public static class Helpers
         return (m, c);
     }
 
+    // Given three collinear points p, q, r, the function checks if
+    // point q lies on line segment 'pr'
+    static Boolean onSegment(Vector2 p, Vector2 q, Vector2 r)
+    {
+        if (q.X <= Math.Max(p.X, r.X) && q.X >= Math.Min(p.X, r.X) &&
+            q.Y <= Math.Max(p.Y, r.Y) && q.Y >= Math.Min(p.Y, r.Y))
+            return true;
+
+        return false;
+    }
+
+    // To find orientation of ordered triplet (p, q, r).
+    // The function returns following values
+    // 0 --> p, q and r are collinear
+    // 1 --> Clockwise
+    // 2 --> Counterclockwise
+    static int orientation(Vector2 p, Vector2 q, Vector2 r)
+    {
+        // See https://www.geeksforgeeks.org/orientation-3-ordered-points/
+        // for details of bel/ow formula.
+        var val = (q.Y - p.Y) * (r.X - q.X) -
+                  (q.X - p.X) * (r.Y - q.Y);
+
+        if (val == 0) return 0; // collinear
+
+        return (val > 0) ? 1 : 2; // clock or counterclock wise
+    }
+
+    // The main function that returns true if line segment 'p1q1'
+    // and 'p2q2' intersect.
     public static bool LineSegmentIntersection(this (Vector2 P1, Vector2 P2) line1, (Vector2 P1, Vector2 P2) line2)
     {
-        var line1AsVector = line1.P2 - line1.P1;
-        var line2ACross = line1AsVector.VectorCross(line2.P1 - line1.P1);
-        var line2BCross = line1AsVector.VectorCross(line2.P2 - line1.P1);
+        var (p1, q1) = line1;
+        var (p2, q2) = line2;
+        // Find the four orientations needed for general and
+        // special cases
+        int o1 = orientation(p1, q1, p2);
+        int o2 = orientation(p1, q1, q2);
+        int o3 = orientation(p2, q2, p1);
+        int o4 = orientation(p2, q2, q1);
 
-        if (line2ACross > 0 && line2BCross > 0 ||
-            line2ACross < 0 &&
-            line2BCross <
-            0) //if both points of one line segment are above or below the other then they cannot intersect.
-            return false;
+        // General case
+        if (o1 != o2 && o3 != o4)
+            return true;
 
-        //If not intersection is not guaranteed so now we check the other way round
+        // Special Cases
+        // p1, q1 and p2 are collinear and p2 lies on segment p1q1
+        if (o1 == 0 && onSegment(p1, p2, q1)) return true;
 
-        var line2AsVector = line2.P2 - line2.P1;
-        var line1ACross = line2AsVector.VectorCross(line1.P1 - line2.P1);
-        var line1BCross = line2AsVector.VectorCross(line1.P2 - line2.P1);
+        // p1, q1 and q2 are collinear and q2 lies on segment p1q1
+        if (o2 == 0 && onSegment(p1, q2, q1)) return true;
 
-        return (!(line1ACross > 0) || !(line1BCross > 0)) && (!(line1ACross < 0) || !(line1BCross < 0));
+        // p2, q2 and p1 are collinear and p1 lies on segment p2q2
+        if (o3 == 0 && onSegment(p2, p1, q2)) return true;
+
+        // p2, q2 and q1 are collinear and q1 lies on segment p2q2
+        if (o4 == 0 && onSegment(p2, q1, q2)) return true;
+
+        return false; // Doesn't fall in any of the above cases
     }
 
     public static bool LineSegmentEllipseIntersection(this (Vector2 P1, Vector2 P2) line,
