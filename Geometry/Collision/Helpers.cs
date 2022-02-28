5b4caed1807893b8506f67734c01dbc504013261
Tim <t.jones@timmoth.com>
2022-02-28 08:03:35 +0000

    Refactored line segment collision

--- Geometry/Collision/Helpers.cs
+++ Geometry/Collision/Helpers.cs
@@ -41,13 +41,10 @@ public static class Helpers
 
     // Given three collinear points p, q, r, the function checks if
     // point q lies on line segment 'pr'
-    static Boolean onSegment(Vector2 p, Vector2 q, Vector2 r)
+    static bool OnSegment(Vector2 p, Vector2 q, Vector2 r)
     {
-        if (q.X <= Math.Max(p.X, r.X) && q.X >= Math.Min(p.X, r.X) &&
-            q.Y <= Math.Max(p.Y, r.Y) && q.Y >= Math.Min(p.Y, r.Y))
-            return true;
-
-        return false;
+        return q.X <= Math.Max(p.X, r.X) && q.X >= Math.Min(p.X, r.X) &&
+               q.Y <= Math.Max(p.Y, r.Y) && q.Y >= Math.Min(p.Y, r.Y);
     }
 
     // To find orientation of ordered triplet (p, q, r).
@@ -55,7 +52,7 @@ public static class Helpers
     // 0 --> p, q and r are collinear
     // 1 --> Clockwise
     // 2 --> Counterclockwise
-    static int orientation(Vector2 p, Vector2 q, Vector2 r)
+    static int Orientation(Vector2 p, Vector2 q, Vector2 r)
     {
         // See https://www.geeksforgeeks.org/orientation-3-ordered-points/
         // for details of bel/ow formula.
@@ -75,10 +72,10 @@ public static class Helpers
         var (p2, q2) = line2;
         // Find the four orientations needed for general and
         // special cases
-        int o1 = orientation(p1, q1, p2);
-        int o2 = orientation(p1, q1, q2);
-        int o3 = orientation(p2, q2, p1);
-        int o4 = orientation(p2, q2, q1);
+        var o1 = Orientation(p1, q1, p2);
+        var o2 = Orientation(p1, q1, q2);
+        var o3 = Orientation(p2, q2, p1);
+        var o4 = Orientation(p2, q2, q1);
 
         // General case
         if (o1 != o2 && o3 != o4)
@@ -86,16 +83,16 @@ public static class Helpers
 
         // Special Cases
         // p1, q1 and p2 are collinear and p2 lies on segment p1q1
-        if (o1 == 0 && onSegment(p1, p2, q1)) return true;
+        if (o1 == 0 && OnSegment(p1, p2, q1)) return true;
 
         // p1, q1 and q2 are collinear and q2 lies on segment p1q1
-        if (o2 == 0 && onSegment(p1, q2, q1)) return true;
+        if (o2 == 0 && OnSegment(p1, q2, q1)) return true;
 
         // p2, q2 and p1 are collinear and p1 lies on segment p2q2
-        if (o3 == 0 && onSegment(p2, p1, q2)) return true;
+        if (o3 == 0 && OnSegment(p2, p1, q2)) return true;
 
         // p2, q2 and q1 are collinear and q1 lies on segment p2q2
-        if (o4 == 0 && onSegment(p2, q1, q2)) return true;
+        if (o4 == 0 && OnSegment(p2, q1, q2)) return true;
 
         return false; // Doesn't fall in any of the above cases
     }
