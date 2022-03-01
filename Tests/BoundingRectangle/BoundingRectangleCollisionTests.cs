666e389fd3cca7965ab0ef0302fbb6fe8d9f9c42
Tim <t.jones@timmoth.com>
2022-03-01 08:06:09 +0000

    Created BoundingRectangle Vector2 Collision Tests

--- Tests/BoundingRectangle/BoundingRectangleCollisionTests.cs
+++ Tests/BoundingRectangle/BoundingRectangleCollisionTests.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Numerics;
+﻿using System.Numerics;
 using Aptacode.Geometry.Collision.Rectangles;
 using Xunit;
 
@@ -8,30 +6,24 @@ namespace Aptacode.Geometry.Tests.Primitives.Equality;
 
 public class BoundingRectangleCollisionTests
 {
-    public class BoundingRectangleCollisionTestDataGenerator : IEnumerable<object[]>
+
+
+    [Theory]
+    [ClassData(typeof(BoundingRectangleBoundingRectangleCollisionTestDataGenerator))]
+    public void BoundingRectangleBoundingRectangleCollision(BoundingRectangle p1, BoundingRectangle p2, bool collides)
     {
-        private readonly List<object[]> _data = new()
-        {
-            new object[] { BoundingRectangle.FromTwoPoints(Vector2.Zero, Vector2.One), BoundingRectangle.FromTwoPoints(Vector2.Zero, Vector2.One), true },
-            new object[] { BoundingRectangle.FromTwoPoints(Vector2.Zero, Vector2.One), BoundingRectangle.FromTwoPoints(Vector2.One, new Vector2(2)), true },
-            new object[] { BoundingRectangle.FromTwoPoints(Vector2.Zero, Vector2.One), BoundingRectangle.FromTwoPoints(new Vector2(2), new Vector2(3)), false },
+        //Arrange
 
-        };
+        //Act
+        var sut = p1.CollidesWith(p2);
 
-        public IEnumerator<object[]> GetEnumerator()
-        {
-            return _data.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
+        //Assert
+        Assert.Equal(collides, sut);
     }
 
     [Theory]
-    [ClassData(typeof(BoundingRectangleCollisionTestDataGenerator))]
-    public void BoundingRectangleCollision(BoundingRectangle p1, BoundingRectangle p2, bool collides)
+    [ClassData(typeof(BoundingRectangleVector2CollisionTestDataGenerator))]
+    public void BoundingRectangleVector2Collision(BoundingRectangle p1, Vector2 p2, bool collides)
     {
         //Arrange
 
---
+++ Tests/BoundingRectangle/BoundingRectangleVector2CollisionTestDataGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Aptacode.Geometry.Collision.Rectangles;
+
+namespace Aptacode.Geometry.Tests.Primitives.Equality;
+
+public class BoundingRectangleVector2CollisionTestDataGenerator : IEnumerable<object[]>
+{
+    private readonly List<object[]> _data = new()
+    {
+        new object[] { BoundingRectangle.FromTwoPoints(Vector2.Zero, Vector2.One), Vector2.One, true },
+        new object[] { BoundingRectangle.FromTwoPoints(Vector2.Zero, Vector2.One), new Vector2(0.5f), true },
+        new object[] { BoundingRectangle.FromTwoPoints(Vector2.Zero, Vector2.One), new Vector2(2), false },
+    };
+
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        return _data.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
