0f1614f24f48c41a531d3f4e9a59ef166df80374
Tim <t.jones@timmoth.com>
2022-03-03 13:36:41 +0000

    Added Triangle creation tests

--- Tests/Primitives/Creation/PolygonCreationTestDataGenerator.cs
+++ Tests/Primitives/Creation/PolygonCreationTestDataGenerator.cs
@@ -10,6 +10,8 @@ public class PolygonCreationTestDataGenerator : IEnumerable<object[]>
     private readonly List<object[]> _data = new()
     {
         new object[] { Polygon.Rectangle.FromTwoPoints(Vector2.Zero, Vector2.One), Vertices.VertexArray.Create(0,0, 0,1, 1,1, 1,0) },
+        new object[] { Polygon.Triangle.Create(Vector2.Zero, Vector2.One, new Vector2(1, 0)), Vertices.VertexArray.Create(0,0, 1,1, 1, 0) },
+        new object[] { Polygon.Create(Vector2.Zero, Vector2.One, new Vector2(1, 0)), Vertices.VertexArray.Create(0,0, 1,1, 1, 0) },
     };
 
     public IEnumerator<object[]> GetEnumerator()
