a89f25f5a31f93d27702a9db4c3c3a2dd12dc072
Tim <t.jones@timmoth.com>
2022-03-04 14:35:32 +0000

    Fixed missing namespace

--- Tests/Primitives/Creation/EllipseCreationTestDataGenerator.cs
+++ Tests/Primitives/Creation/EllipseCreationTestDataGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using Aptacode.Geometry.Primitives;
 
 namespace Aptacode.Geometry.Tests.Primitives.Creation;
