28508726ecea1efc4419ef09a1d2424f50377394
ryanm <ryan.mason95@hotmail.co.uk>
2022-03-07 12:09:47 +0000

    Added ellipse standard form test

---
+++ Tests/Primitives/EllipseStandardForm/EllipseStandardformTests.cs
@@ -0,0 +1,24 @@
+﻿using Aptacode.Geometry.Primitives;
+using System.Numerics;
+using Xunit;
+
+namespace Aptacode.Geometry.Tests.Primitives.EllipseStandardForm
+{
+    public class EllipseStandardformTests
+    {
+        [Theory]
+        [ClassData(typeof(EllipseStandardFormTestDataGenerator))]
+        public void TestStandardForm(Ellipse ellipse, float a, float b, float c, float d, float e, float f)
+        {
+            //Arrange
+            //Act
+            //Assert
+            Assert.Equal(a, ellipse.StandardForm.A);
+            Assert.Equal(b, ellipse.StandardForm.B);
+            Assert.Equal(c, ellipse.StandardForm.C);
+            Assert.Equal(d, ellipse.StandardForm.D);
+            Assert.Equal(e, ellipse.StandardForm.E);
+            Assert.Equal(f, ellipse.StandardForm.F);
+        }
+    }
+}
