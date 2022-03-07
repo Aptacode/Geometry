8f6535edd52659227c81462bb7f5c62ecfd055b2
Tim <t.jones@timmoth.com>
2022-03-07 22:26:37 +0000

    Refactored Ellipse methods into EllipseExtensions

--- Geometry/Primitives/Ellipse.cs
+++ Geometry/Primitives/Ellipse.cs
@@ -75,7 +75,7 @@ public sealed class Ellipse : Primitive
         var position = new Vector2(x, y);
         var radii = new Vector2(a, b);
         var vertexArray = VertexArray.Create(position);
-        var boundingRectangle = GetBoundingRectangle(position, radii, rotation);
+        var boundingRectangle = EllipseExtensions.GetBoundingRectangle(position, radii, rotation);
 
         return new Ellipse(vertexArray, boundingRectangle, radii, rotation);
     }
@@ -83,7 +83,8 @@ public sealed class Ellipse : Primitive
     public static Ellipse Create(Vector2 position, Vector2 radii, float rotation)
     {
         var vertexArray = VertexArray.Create(position);
-        var boundingRectangle = GetBoundingRectangle(position, radii, rotation);
+        var boundingRectangle = EllipseExtensions.GetBoundingRectangle(position, radii, rotation);
+
         return new Ellipse(vertexArray, boundingRectangle, radii, rotation);
     }
 
@@ -174,7 +175,7 @@ public sealed class Ellipse : Primitive
     public override Ellipse ScaleAboutCenter(Vector2 delta)
     {
         Radii *= delta;
-        BoundingRectangle = GetBoundingRectangle(Position, Radii, Rotation);
+        BoundingRectangle = EllipseExtensions.GetBoundingRectangle(Position, Radii, Rotation);
         _updateFoci = _updateStandardForm = true;
 
         return this;
@@ -184,7 +185,7 @@ public sealed class Ellipse : Primitive
     {
         //Todo
 
-        BoundingRectangle = GetBoundingRectangle(Position, Radii, Rotation);
+        BoundingRectangle = EllipseExtensions.GetBoundingRectangle(Position, Radii, Rotation);
         _updateFoci = _updateStandardForm = true;
 
         return this;
@@ -194,7 +195,7 @@ public sealed class Ellipse : Primitive
     {
         //Todo
 
-        BoundingRectangle = GetBoundingRectangle(Position, Radii, Rotation);
+        BoundingRectangle = EllipseExtensions.GetBoundingRectangle(Position, Radii, Rotation);
         _updateFoci = _updateStandardForm = true;
 
         return this;
@@ -204,7 +205,7 @@ public sealed class Ellipse : Primitive
     {
         //Todo
 
-        BoundingRectangle = GetBoundingRectangle(Position, Radii, Rotation);
+        BoundingRectangle = EllipseExtensions.GetBoundingRectangle(Position, Radii, Rotation);
         _updateFoci = _updateStandardForm = true;
 
         return this;
@@ -235,105 +236,6 @@ public sealed class Ellipse : Primitive
         }
 
         return (Position, Position);
-    }
-
-    public static (double u0, double u1, double u2, double u3, double u4) GetResultantPolynomial(double A1,
-        double B1, double C1, double D1, double E1, double F1, double A2, double B2, double C2, double D2,
-        double E2, double F2) //Takes the coefficients of 2 ellipses (conics) and returns the Bezout determinant R(y) = u0 + u1y + u2y^2 + u3y^3 + u4y^4, is is the equation for the solutions for the y points of intersection, they may be complex.
-    {
-        var v0 = A1 * B2 - A2 * B1;
-        var v1 = A1 * C2 - A2 * C1;
-        var v2 = A1 * D2 - A2 * D1;
-        var v3 = A1 * E2 - A2 * E1;
-        var v4 = A1 * F2 - A2 * F1;
-        var v5 = B1 * C2 - B2 * C1;
-        var v6 = B1 * E2 - B2 * E1;
-        var v7 = B1 * F2 - B2 * F1;
-        var v8 = C1 * D2 - C2 * D1;
-        var v9 = D1 * E2 - D2 * E1;
-        var v10 = D1 * F2 - D2 * F1;
-
-        var u0 = v2 * v10 - v4 * v4;
-        var u1 = v0 * v10 + v2 * (v7 + v9) - 2 * v3 * v4;
-        var u2 = v0 * (v7 + v9) + v2 * (v6 - v8) - v3 * v3 - 2 * v1 * v4;
-        var u3 = v0 * (v6 - v8) + v2 * v5 - 2 * v1 * v3;
-        var u4 = v0 * v5 - v1 * v1;
-
-        return (u0, u1, u2, u3, u4);
-    }
-
-    public static bool QuarticHasRealRoots(double u0, double u1, double u2, double u3, double u4)
-    {
-        if (Math.Abs(u4) < Constants.Tolerance && Math.Abs(u3) > Constants.Tolerance)
-        {
-            return true;
-        }
-
-        if (Math.Abs(u4) < Constants.Tolerance && Math.Abs(u3) < Constants.Tolerance && Math.Abs(u2) < Constants.Tolerance)
-        {
-            var det = u1 * u1 - 4 * u2 * u0;
-            if (Math.Abs(det) < Constants.Tolerance)
-            {
-                return true;
-            }
-
-            return det >= 0;
-        }
-
-        var delta = 256 * u4 * u4 * u4 * u0 * u0 * u0
-                    - 192 * u4 * u4 * u3 * u1 * u0 * u0
-                    - 128 * u4 * u4 * u2 * u2 * u0 * u0
-                    + 144 * u4 * u4 * u2 * u1 * u1 * u0
-                    - 27 * u4 * u4 * u1 * u1 * u1 * u1
-                    + 144 * u4 * u3 * u3 * u1 * u0 * u0
-                    - 6 * u4 * u3 * u3 * u1 * u1 * u0
-                    - 80 * u4 * u3 * u2 * u2 * u1 * u0
-                    + 18 * u4 * u3 * u2 * u1 * u1 * u1
-                    + 16 * u4 * u2 * u2 * u2 * u2 * u0
-                    - 4 * u4 * u2 * u2 * u2 * u1 * u1
-                    - 27 * u3 * u3 * u3 * u3 * u0 * u0
-                    + 18 * u3 * u3 * u3 * u2 * u1 * u0
-                    - 4 * u3 * u3 * u3 * u1 * u1 * u1
-                    - 4 * u3 * u3 * u2 * u2 * u2 * u0
-                    + u3 * u3 * u2 * u2 * u1 * u1;
-
-        if (delta < 0 && Math.Abs(delta) > 0.01f)
-        {
-            return true;
-        }
-
-        var p = 8 * u4 * u2 - 3 * u3 * u3;
-        var d = 64 * u4 * u4 * u4 * u0
-                - 16 * u4 * u4 * u2 * u2
-                + 16 * u4 * u3 * u3 * u2
-                - 16 * u4 * u4 * u3 * u1
-                - 3 * u3 * u3 * u3 * u3;
-
-        return (p <= 0 || Math.Abs(p) <= Constants.Tolerance) && d <= 0;
-    }
-
-    private static BoundingRectangle GetBoundingRectangle(Vector2 position, Vector2 radii, float rotation)
-    {
-        var asquared = radii.X * radii.X;
-        var bsquared = radii.Y * radii.Y;
-
-        var costheta = Math.Cos(rotation);
-        var costhetasquared = costheta * costheta;
-
-        var sintheta = Math.Sin(rotation);
-        var sinthetasquared = sintheta * sintheta;
-
-        var xdelta = (float)Math.Sqrt(asquared * costhetasquared + bsquared * sinthetasquared);
-        var ydelta = (float)Math.Sqrt(asquared * sinthetasquared + bsquared * costhetasquared);
-        var delta = new Vector2(xdelta, ydelta);
-        var bottomLeft = position - delta;
-        var topRight = position + delta;
-
-        return new BoundingRectangle
-        {
-            BottomLeft = bottomLeft,
-            TopRight = topRight
-        };
     }
 
     private (double A, double B, double C, double D, double E, double F)
